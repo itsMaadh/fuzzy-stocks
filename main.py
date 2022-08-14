1e15af3aed68094b6f33be0b0725676f27eb139f
Maadh <maadh@protonmail.ch>
2022-08-14 16:35:06 +0800

    fix: linting errors and comments

--- main.py
+++ main.py
@@ -1,48 +1,54 @@
 import numpy as np
-import matplotlib.pyplot as plt
-def increasing(x,a,b):
-    if(x<=a):
-        r=0
-    elif(x>=b):
-        r=1
+
+
+def increasing(x, a, b):
+    if x <= a:
+        r = 0
+    elif x >= b:
+        r = 1
     else:
-        r=(x-a)/(b-a)
+        r = (x - a) / (b - a)
     return r
-def gauss(x,u,s):
+
+
+def gauss(x, u, s):
     r = np.exp(((-(x - u) ** 2) / (2 * s ** 2)))
-    return(r)
+    return r
 
-def gauss1(x,u,s):
-    if (x <= u):
+
+def gauss1(x, u, s):
+    if x <= u:
         r = 1
     else:
         r = np.exp(((-(x - u) ** 2) / (2 * s ** 2)))
-    return(r)
+    return r
 
-def gauss2(x,u,s):
-    if (x >= u):
+
+def gauss2(x, u, s):
+    if x >= u:
         r = 1
     else:
         r = np.exp(((-(x - u) ** 2) / (2 * s ** 2)))
-    return(r)
-
-def decreasing(x,a,b):
-    if (x <= a):
-        r = 1
-    elif (x >= b):
-        r = 0
-    else:
-        r = (b-x) / (b - a)
     return r
 
-def trape(x,a,b,c,d):
-    if (x <= a or x>=d):
-        r = 0
-    elif (x >= b and x<=c):
+
+def decreasing(x, a, b):
+    if x <= a:
         r = 1
-    elif (x> a and x<b):
-        r = (x-a) /(b - a)
+    elif x >= b:
+        r = 0
     else:
-        r = (d-x)/(d-c)
+        r = (b - x) / (b - a)
     return r
 
+
+def trape(x, a, b, c, d):
+    if x <= a or x >= d:
+        r = 0
+    elif b <= x <= c:
+        r = 1
+    elif a < x < b:
+        r = (x - a) / (b - a)
+    else:
+        r = (d - x) / (d - c)
+    return r
