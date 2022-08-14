36f83d114e39738ae17d1b70e85e86d160a37bdd
Maadh <maadh@protonmail.ch>
2022-08-14 16:41:20 +0800

    fix: minor fix for readability

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Please refer to the 9 rules and membership function in the submitted assignment file
 
 import numpy as np
-import main
+import main as membership
 import matplotlib.pyplot as plt
 
 # Defining the Memberships
@@ -12,45 +12,45 @@ low_PE = np.zeros_like(PE)
 med_PE = np.zeros_like(PE)
 high_PE = np.zeros_like(PE)
 for i in range(len(PE)):
-    low_PE[i] = main.decreasing(PE[i], 0, 10)
-    med_PE[i] = main.trape(PE[i], 8, 20, 20, 30)
-    high_PE[i] = main.increasing(PE[i], 20, 30)
+    low_PE[i] = membership.decreasing(PE[i], 0, 10)
+    med_PE[i] = membership.trape(PE[i], 8, 20, 20, 30)
+    high_PE[i] = membership.increasing(PE[i], 20, 30)
 
 ROE = np.linspace(0, 30, 200)
 poor_ROE = np.zeros_like(ROE)
 ave_ROE = np.zeros_like(ROE)
 high_ROE = np.zeros_like(ROE)
 for i in range(len(ROE)):
-    poor_ROE[i] = main.decreasing(ROE[i], 5, 7)
-    ave_ROE[i] = main.trape(ROE[i], 5, 9, 9, 13)
-    high_ROE[i] = main.increasing(ROE[i], 10, 15)
+    poor_ROE[i] = membership.decreasing(ROE[i], 5, 7)
+    ave_ROE[i] = membership.trape(ROE[i], 5, 9, 9, 13)
+    high_ROE[i] = membership.increasing(ROE[i], 10, 15)
 
 GPP = np.linspace(-1, 1, 100)
 Neg_GPP = np.zeros_like(GPP)
 Neu_GPP = np.zeros_like(GPP)
 Pos_GPP = np.zeros_like(GPP)
 for i in range(len(GPP)):
-    Neg_GPP[i] = main.gauss1(GPP[i], -1, 0.25)
-    Neu_GPP[i] = main.gauss(GPP[i], 0, 0.16)
-    Pos_GPP[i] = main.gauss2(GPP[i], 1, 0.25)
+    Neg_GPP[i] = membership.gauss1(GPP[i], -1, 0.25)
+    Neu_GPP[i] = membership.gauss(GPP[i], 0, 0.16)
+    Pos_GPP[i] = membership.gauss2(GPP[i], 1, 0.25)
 
 MAJEC = np.linspace(-3, 3, 200)
 Dec_MAJEC = np.zeros_like(MAJEC)
 Const_MAJEC = np.zeros_like(MAJEC)
 Inc_MAJEC = np.zeros_like(MAJEC)
 for i in range(len(MAJEC)):
-    Dec_MAJEC[i] = main.decreasing(MAJEC[i], -1, 0)
-    Const_MAJEC[i] = main.trape(MAJEC[i], -1, 0, 0, 1)
-    Inc_MAJEC[i] = main.increasing(MAJEC[i], 0, 1)
+    Dec_MAJEC[i] = membership.decreasing(MAJEC[i], -1, 0)
+    Const_MAJEC[i] = membership.trape(MAJEC[i], -1, 0, 0, 1)
+    Inc_MAJEC[i] = membership.increasing(MAJEC[i], 0, 1)
 
 SPM = np.linspace(-8, 8, 100)
 Red_SPM = np.zeros_like(SPM)
 Const_SPM = np.zeros_like(SPM)
 Inc_SPM = np.zeros_like(SPM)
 for i in range(len(SPM)):
-    Red_SPM[i] = main.gauss1(SPM[i], -5, 0.6)
-    Const_SPM[i] = main.gauss(SPM[i], 0, 1.9)
-    Inc_SPM[i] = main.gauss2(SPM[i], 5, 0.6)
+    Red_SPM[i] = membership.gauss1(SPM[i], -5, 0.6)
+    Const_SPM[i] = membership.gauss(SPM[i], 0, 1.9)
+    Inc_SPM[i] = membership.gauss2(SPM[i], 5, 0.6)
 
 # Plots
 plt.figure(0)
